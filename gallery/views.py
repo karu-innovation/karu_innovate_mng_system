cd95c3f3cace7bccd2e5f55accc28d517427d66d
Gamer Snave <snaveqiptoo@gmail.com>
2022-11-20 22:43:55 +0300

    Snave added image upload View

--- gallery/views.py
+++ gallery/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def GalleryView(request):
     context={}
     return render (request,'gallery/gallery.html',context)
+
+# Image Upload
+def ImageUploadView(request):
+    context={}
+    return render (request,'gallery/image_upload.html',context)
+    
