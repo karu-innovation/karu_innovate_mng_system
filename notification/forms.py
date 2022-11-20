9029f96a568abd3fd23d83fa25ae9fe6f6668d29
Kiash254 <codewithkiash@gmail.com>
2022-11-20 22:47:51 +0300

    fixed notifications

--- notification/forms.py
+++ notification/forms.py
@@ -2,8 +2,12 @@ from django import forms
 
 class EmailForm(forms.Form):
     email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+    subject = forms.CharField(max_length=100)
     widget = {
-        'email':forms.EmailInput(attrs={'class':'form-control'})
+        'email':forms.EmailInput(attrs={'class':'form-control'}),
+        'message':forms.Textarea(attrs={'class':'form-control'}),
+        'subject':forms.TextInput(attrs={'class':'form-control'})
     }
     
 class TextForm(forms.Form):
